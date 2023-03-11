5e34d140bd91c8479e3c5c9604e05cd56d1c8b74
StringNameEqualsThejazzman <115343589+StringNameEqualsThejazzman@users.noreply.github.com>
2023-03-11 09:44:36 +0100

    gestion des erreurs

--- CatFact/MainPage.xaml.cs
+++ CatFact/MainPage.xaml.cs
@@ -12,25 +12,44 @@ public partial class MainPage : ContentPage
 		InitializeComponent();
 	}
 
-    //méthode async qui permet de se connecter à l'api et de récuperer le JSON
-	private async void LoadCatFact()
+    //----------------méthode async qui permet de se connecter à l'api et de récuperer le JSON----------------
+    private async void LoadCatFact()
 	{
-		//Je crée le client HTTP
-        using var client = new HttpClient();
+        //gestion des exceptions
+        try
+        {
+            //Je crée le client HTTP
+            using var client = new HttpClient();
 
-		//Je construit le chemin de l'url pour récupérer un fait sur les chats
-        var apiUrl = $"{ApiBaseUrl}/fact";
+            //Je construit le chemin de l'url pour récupérer un fait sur les chats
+            var apiUrl = $"{ApiBaseUrl}/fact";
 
-        //Envoie d'une requête GET pour récuperer le fichier JSON
-        var response = await client.GetAsync(apiUrl);
-        response.EnsureSuccessStatusCode();
-        var jsonString = await response.Content.ReadAsStringAsync();
+            //Envoie d'une requête GET pour récuperer le fichier JSON
+            var response = await client.GetAsync(apiUrl);
+            response.EnsureSuccessStatusCode();
+            var jsonString = await response.Content.ReadAsStringAsync();
 
-        //deserialisation du fuchier Json en un obejt
-        var catFactResponse = JsonSerializer.Deserialize<CatFactResponse>(jsonString);
+            //deserialisation du fuchier Json en un obejt
+            var catFactResponse = JsonSerializer.Deserialize<CatFactResponse>(jsonString);
 
-        //Mise à jour de l'affichage client
-        CatFactLabel.Text = catFactResponse.Fact;
+            //Mise à jour de l'affichage client
+            CatFactLabel.Text = catFactResponse.Fact;
+        }
+        catch (HttpRequestException ex)
+        {
+            CatFactLabel.Text = "Erreur lors de la récupération du fait sur les chats.";
+            Console.WriteLine($"Erreur lors de la récupération du fait sur les chats : {ex.Message}");
+        }
+        catch (JsonException ex)
+        {
+            CatFactLabel.Text = "Erreur lors de la récupération du fait sur les chats.";
+            Console.WriteLine($"Erreur de la désérialisation du fichier JSON : {ex.Message}");
+        }
+        catch (Exception ex)
+        {
+            CatFactLabel.Text = "Erreur lors de la récupération du fait sur les chats.";
+            Console.WriteLine($"Erreur : {ex.Message}");
+        }
     }
 }
 
